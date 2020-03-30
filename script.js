cb3910c57c8c99bb070005b49108c13a7778e6ce
Tyler McBride <tymcbrid@gmail.com>
2020-03-30 12:05:32 -0700

    8 - 128 update

--- script.js
+++ script.js
@@ -10,19 +10,35 @@ var chooseArray = [lowerlettersArray, upperlettersArray, numbersArray, specialAr
 
 
 // Criteria Questions
+// Series of prompts for password criteria
+// Password criteria prompt and I select which criteria to include in the password
+// Length prompt
+// ex. at least 8 characters and no more than 128 characters
+// Character type prompt
+// ex. lowercase, uppercase, numeric, and/or special characters
+// Input validation prompt and make sure at least one character type is selected
+// Password generated
+// Password displayed in an alert or written to the page
+
 var lengthCriteria = confirm("Would you like to specify password length?");
 if (lengthCriteria == true) {
-    var passwordminLength = prompt("Please enter a minimum length for your password");
-    var passwordmaxLength = prompt("Please enter a maximum length for your password");
+    var passwordminLength = prompt("Please enter a minimum length for your password that is between 8 and 128");
+    while(parseInt(passwordminLength) < 8 || parseInt(passwordminLength) > 128) {
+        var passwordminLength = prompt("Please enter a minimum length for your password that is between 8 and 128");
+    };
+    var passwordmaxLength = prompt("Please enter a maximum length for your password that is between 8 and 128 and larger than the minimum length");
+    while(parseInt(passwordmaxLength) > 128 || parseInt(passwordmaxLength) < parseInt(passwordminLength)) {
+        var passwordmaxLength = prompt("Please enter a maximum length for your password that is between 8 and 128 and larger than the minimum length");
+    };
     var passwordLengthRange = passwordmaxLength - passwordminLength + 1;
-    var passwordAdditionalLength = Math.floor(Math.random() * passwordLengthRange);
-    var passwordLength = parseInt(passwordminLength) + parseInt(passwordAdditionalLength);
     // console.log("max length = " + passwordmaxLength);
     // console.log("min length = " + passwordminLength);
     // console.log("length range = " + passwordLengthRange);
     // console.log("random additional length = " + passwordAdditionalLength);
     // console.log("password length = " + passwordLength);
 }
+
+
 var typeCriteria = confirm("Would you like to specify password character types?");
 if (typeCriteria == true) {
     var typeLower = confirm("Would you like to use lowercase character types?");
@@ -30,7 +46,7 @@ if (typeCriteria == true) {
     var typeNumeric = confirm("Would you like to use numeric character types?");
     var typeSpecial = confirm("Would you like to use special character types?");
 }
-
+// Which selections you made
 if (typeCriteria == true) {
     var chooseArray =[];
     console.log("length is " + chooseArray.length);
@@ -52,79 +68,36 @@ if (typeCriteria == true) {
 // based on that input and returns it
 function generatePassword() {
     var passwordAdditionalLength = Math.floor(Math.random() * passwordLengthRange);
-    var passwordLength = parseInt(passwordminLength) + parseInt(passwordAdditionalLength);
+    // var passwordLength = parseInt(passwordminLength) + parseInt(passwordAdditionalLength);
     var j = 10;
     if (lengthCriteria == true) {
         var j = passwordLength;
     }
-    
-    // Will produce 10-i random numbers
+
     for (var i = 0; i < j; i++) {
+    var num = Math.floor(Math.random() * chooseArray.length);
 
-    var arrayLength = chooseArray.length
-    var num = Math.floor(Math.random() * arrayLength);
-    // var choice = chooseArray[num];
-
-    console.log(num)
-    
-    
-// instead of all the combinations just figure out how to remove from the original array
-
-// something broke with choose array, last pull was working
-
-
-    if (num == 0) {
-        var arrayChoice = chooseArray[0]
-        var choiceLength = arrayChoice.length
-        var selection = Math.floor(Math.random() * choiceLength);
-        var choice = arrayChoice[selection]
-    }
-    if (num == 1) {
-        var arrayChoice = chooseArray[1]
-        var choiceLength = arrayChoice.length
-        var selection = Math.floor(Math.random() * choiceLength);
-        var choice = arrayChoice[selection]
-    }
-    if (num == 2) {
-        var arrayChoice = chooseArray[2]
-        var choiceLength = arrayChoice.length
-        var selection = Math.floor(Math.random() * choiceLength);
-        var choice = arrayChoice[selection]
-    }
-    if (num == 3) {
-        var arrayChoice = chooseArray[3]
-        var choiceLength = arrayChoice.length
-        var selection = Math.floor(Math.random() * choiceLength);
-        var choice = arrayChoice[selection]
-    }
-    console.log(choice);
-
-    var myfirstPassword = myfirstPassword + choice;
-    console.log(myfirstPassword)
+        for (var k = 0; k <= num; k++) {
+            if (k == num){
+            var arrayChoice = chooseArray[num]
+            var choiceLength = arrayChoice.length
+            var selection = Math.floor(Math.random() * choiceLength);
+            var choice = arrayChoice[selection]
+            // console.log(choice);
+            }
+        }
+        var myfirstPassword = myfirstPassword + choice;
+        // console.log(myfirstPassword)
     }
     var newlength = j + 9;
     var myPassword = myfirstPassword.slice(9, newlength);
-    console.log(myPassword);
-    console.log(passwordLength)
-
-
-    var finalPass = document.querySelector("#password").select();
+    // console.log(myPassword);
+    // console.log(passwordLength)
     document.querySelector("#previousGenerated").innerHTML += "<li>" + myPassword + "</li>" + "<br>";
-
     return myPassword;
-
-
-   // Generate random number between 0-9
-    // var num = Math.floor(Math.random() * 10);
-
-
-
   }
   
-  //////////////////////////////////////////////////////////////
-  // DO NOT TOUCH THE CODE BELOW
-  //////////////////////////////////////////////////////////////
-  // Assignment Code
+  // Assignment Pre-generated Code
   var generateBtn = document.querySelector("#generate");
   
   // Write password to the #password input
@@ -138,18 +111,7 @@ function generatePassword() {
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
 
-
-// Series of prompts for password criteria
-// Password criteria prompt and I select which criteria to include in the password
-// Length prompt
-// ex. at least 8 characters and no more than 128 characters
-// Character type prompt
-// ex. lowercase, uppercase, numeric, and/or special characters
-// Input validation prompt and make sure at least one character type is selected
-// Password generated
-// Password displayed in an alert or written to the page
-
-
+  // Copy button
 var copyBtn = document.querySelector("#copy");
 
 function exectureCopy() {
@@ -157,6 +119,4 @@ function exectureCopy() {
     document.execCommand("Copy");
     alert("Password is copied to clipboard");
 }
-
 copyBtn.addEventListener("click", exectureCopy);
-// document.execcommand("Copy")
